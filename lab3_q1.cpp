9db4fbd19bb8a0c3a8626d9c15bf733acb7e9fb4
Lipsagamango <42207107+Lipsagamango@users.noreply.github.com>
2018-08-17 11:54:57 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+int a;
+float b;
+double c;
+char d;
+bool e;
+cout << "size of int is " << sizeof(a) << endl;
+cout << "size of float is" << sizeof(b) << endl;
+cout << "size of double is" << sizeof(c) << endl;
+cout << "size of char is" << sizeof(d) << endl;
+cout << "size of boolean is" << sizeof(e) << endl;
+return 0;
+}
